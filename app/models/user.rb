9fde0c05c0d35cb19e56f4267b27c33456ab0ffa
dai03 <d.taniguchi0803@gmail.com>
2020-04-05 11:00:45 +0000

    sign up validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 15 }
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    validates :password, presence: true, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }
 end

c766effdd0fe2b3b49cd98b99a2cef120f32c8a9
dai03 <d.taniguchi0803@gmail.com>
2020-04-05 15:55:13 +0000

    session on the way

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#index'
@@ -6,5 +7,11 @@ Rails.application.routes.draw do
   get 'pages/help'
   
   resources :users
+  
+  get '/login', to: 'sessions#new'
+  
+  post '/login', to: 'sessions#create'
+  
+  delete '/logout', to: 'sessions#destroy'
   
 end
